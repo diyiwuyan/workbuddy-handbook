7d08175677bd61f577fe126596282342b5d70472
王小仙 <wangxiaoxian@192.168.1.16>
2026-07-16 12:52:30 +0800

    chore: add .gitignore, exclude build output

--- out/assets/chunks/001_CleanShot_202026-07-10_20at_2000.14.28_2x_Lxy0b4axOo.Bt0dHdB3.Ljlpp_dt.js
+++
@@ -1,1 +0,0 @@
-const _="/assets/001_CleanShot_202026-07-10_20at_2000.14.28_2x_Lxy0b4axOo.Bt0dHdB3.png";export{_};
--- out/assets/chunks/001_image_AXyxbhbcqo.C-sVVDrO.DIB4xoLc.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/001_image_AXyxbhbcqo.C-sVVDrO.png";export{s as _};
